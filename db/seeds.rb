78f5b760587c5de11e2e729c6eb8dc88a71a81bd
Jonathan Chang <jonoschang7@gmail.com>
2014-05-02 17:53:26 -0400

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Plan.create(
+	price: 40,
+	minutes: -1,
+	data: 3000,
+	text: nil
+	)
+
+Plan.create(
+	price: 45,
+	minutes: -1,
+	data: 2000,
+	text: nil
+	)
+
+Plan.create(
+	price: 65,
+	minutes: -1,
+	data: 2000,
+	text: -1
+	)
+
+Plan.create(
+	price: 67,
+	minutes: -1,
+	data: 6000,
+	text: nil
+	)
+
+Plan.create(
+	price: 30.5,
+	minutes: 250,
+	data: 150,
+	text: nil
+	)
+
+Plan.create(
+	price: 59,
+	minutes: 200,
+	data: 6000,
+	text: -1
+	)
+
+Plan.create(
+	price: 75,
+	minutes: -1,
+	data: 6000,
+	text: -1
+	)
+
+Plan.create(
+	price: 54,
+	minutes: 450,
+	data: 6000,
+	text: -1
+	)
