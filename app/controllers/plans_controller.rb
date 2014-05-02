f7408d774260b2f605a18b82a9440c66e000c9c7
Jonathan Chang <jonoschang7@gmail.com>
2014-05-02 15:07:36 -0400

    Work on index, show, new action for plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,17 @@
 class PlansController < ApplicationController
 	def index
+		@plans = Plan.all
+	end
+
+	def show
+		@plan = Plan.find(params[:id])
+	end
+
+	def new
+		@plan = Plan.new
+	end
+
+	def create
+		
 	end
 end
