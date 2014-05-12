bb938f77f6a90caa702cb09d89b7a6893d21ef28
Philip Jun <inbox@philipjun.com>
2014-05-12 15:06:11 -0400

    updated index page

---
+++ userstories.rb
@@ -0,0 +1,29 @@
+As a User I want to...
+
+- apply filters and search plans 
+-register/login to see more than 5 cellphone plans
+-view selected plans
+# -view top rated plans by users
+-show users savings calculator
+
+# -post reviews for plans
+-see plan success rates
+-register for a new account
+# -send plan request to dealers
+-submit my own plans to the website
+-save plans on my profile
+
+-share my plans to friends via Social Media networks
+-be awarded for sharing my plans>gamification 
+-search nearby dealer networks via google maps
+
+
+# As a dealer I want to...
+# -register
+# -view incoming plans and sort by details
+# -view past deals
+# -send counteroffer plans and details
+# -send messages to the customer
+# -view location of customers
+
+
